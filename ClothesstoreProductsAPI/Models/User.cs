94b8cdbd37b7e7e8420c3a46c22a4a0dbe2d8bda
grusnny <37426267+grusnny@users.noreply.github.com>
2021-02-24 15:32:56 -0500

    Initial commit

---
+++ ClothesstoreProductsAPI/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClothesstoreProductsAPI.Models
+{
+    public class User
+    {
+
+        public string UserId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+    }
+}
