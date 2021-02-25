04d86be7268ec8fb50b1fc848b4d3eed85f975f1
grusnny <37426267+grusnny@users.noreply.github.com>
2021-02-24 20:38:06 -0500

    Reset

---
+++ XUnitTestClothesstore/Fixture/ApiClothesstoreFixture.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Alba;
+
+namespace XUnitTestClothesstore.Fixture
+{
+    public class ApiClothesstoreFixture : IDisposable
+    {
+        public readonly SystemUnderTest systemUnderTest;
+        public ApiClothesstoreFixture()
+        {
+            systemUnderTest = SystemUnderTest.ForStartup<ClothesstoreProductsAPI.Startup>();
+        }
+        public void Dispose()
+        {
+            systemUnderTest?.Dispose();
+        }
+    }
+}
